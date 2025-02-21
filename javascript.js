4d9d3f2915023d51148358541b5128782ba3231a
Denis Le <dl.denisle@gmail.com>
2025-02-21 16:04:48 -0500

    Add scoreboard

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     // choose 0, 1, or 2
     let random = Math.floor(Math.random() * 3);
@@ -13,51 +16,53 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice() {
-    let selection = prompt("Rock, Paper, or Scissors?");
-    return selection.toUpperCase();
-}
-
 function playRound(playerSelection, computerSelection) {
-    
-    console.log(`Player Choice: ${playerSelection}`);
-    console.log(`Computer Choice: ${computerSelection}`);
 
     if (playerSelection == "ROCK") {
         if (computerSelection == "ROCK") {
             return ("Tie!");
         }
         else if (computerSelection == "PAPER") {
+            computerScore++;
             return ("You Lose!");
         }
         else {
+            playerScore++;
             return ("You Win!");
         }
     }
 
     if (playerSelection == "PAPER") {
         if (computerSelection == "ROCK") {
+            playerScore++;
             return ("You Win!");
         }
         else if (computerSelection == "PAPER") {
             return ("Tie!");
         }
         else {
+            computerScore++;
             return ("You Lose!");
         }
     }
 
     if (playerSelection == "SCISSORS") {
         if (computerSelection == "ROCK") {
+            computerScore++;
             return ("You Lose!");
         }
         else if (computerSelection == "PAPER") {
+            playerScore++;
             return ("You Win!");
         }
         else {
             return ("Tie!");
         }
     }
+
+}
+
+function score(result) {
 
 }
 
@@ -71,7 +76,9 @@ function playGame() {
     let playerSelection = 'ROCK';
     let computerSelection = getComputerChoice();
 
+    const scoreboard = document.querySelector('.scoreboard');
     const buttons = document.querySelector('#buttons');
+    const results = document.querySelector('.results');
 
     buttons.addEventListener('click', (event) => {
         let target = event.target;
@@ -91,7 +98,18 @@ function playGame() {
                 break;
         }
 
-        console.log(playRound(playerSelection, computerSelection))
+        const result = document.createElement('p');
+        const choices = document.createElement('p');
+        const scores = document.createElement('p');
+
+        choices.textContent = `Player Choice: ${playerSelection}, Computer Choice: ${computerSelection}`;
+        result.textContent = playRound(playerSelection, computerSelection);
+        scores.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+
+        results.appendChild(choices);
+        results.appendChild(result);
+        results.appendChild(scores);
+
     });
 }
 
